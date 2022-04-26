0e2e43b6b9a4aa56a6b3dce2965082a92823a4ed
Murtaxa-Baig <darvaish49@gmaile.com>
2022-04-26 21:21:26 +0500

    first react practice site without resposive

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Part4 from './component/part4/Part4';
 import Part5 from './component/part5/Part5';
 import Part6 from './component/part6/Part6';
 import Part7 from './component/part7/Part7';
+import Footer from './component/footer/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Part5 />
       <Part6 />
       <Part7 />
+      <Footer />
     </div>
   );
 }
